2c619e6cd243d0f5ec32443d00f5cfd9f6792d10
Hmaker <513193084@qq.com>
2020-12-13 14:47:39 +0800

    Update the README  and  push the first cpp source code

---
+++ Chapter2/myfirst.cpp
@@ -0,0 +1,14 @@
+/* myfirst.cpp  -- display a message */
+
+#include <iostream>			//a PREPROCEWSIR directive
+
+using namespace std;
+
+/* main function header*/
+int main()
+{
+	cout << "Welcome to C++ World";
+	cout << endl;     // Start a New Line
+	cout << "You won't regret it!" << endl; //More output 
+	return 0;
+} 
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # cpp_tutorials
+
+## 摘要
+  源自《C++ Primer Plus》的学习总结。
