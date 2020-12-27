960346e9d8a95f337c72e21d7b2528fcb7cf095f
Hmaker <513193084@qq.com>
2020-12-27 12:36:17 +0800

    Add Chapter9

---
+++ Chapter9/file2.cpp
@@ -0,0 +1,23 @@
+// file2.cpp -- contains functions called in file1.cpp
+#include <iostream>
+#include <cmath>
+#include "coordin.h"
+
+using namespace std;
+
+polar rect_to_polar(rect xypos)
+{
+	polar answer;
+	answer.distance = sqrt(xypos.x * xypos.x + xypos.y * xypos.y);
+	answer.angle = atan2(xypos.y, xypos.x);
+	return answer;
+}
+
+void show_polar(polar dapos)
+{
+	const double Rad_to_deg = 57.2957951;
+	
+	cout << "distance = " << dapos.distance;
+	cout << ", angle = " << dapos.angle * Rad_to_deg;
+	cout << " degrees\n";
+}
